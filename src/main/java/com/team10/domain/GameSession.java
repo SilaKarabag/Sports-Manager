f1af0cedd0a3f037f53a1230f62841bf75b7c76f
Laman679 <laman999mammadova@gmail.com>
2026-04-11 23:27:16 +0300

    Implemented game session

---
+++ src/main/java/com/team10/domain/GameSession.java
@@ -0,0 +1,47 @@
+package com.team10.domain;
+
+import com.team10.sports.Sport;
+import java.util.Objects;
+
+public class GameSession {
+
+    private Sport sport;
+    private League league;
+    private Team managedTeam;
+
+    public void startSeason() {
+        if (sport == null || league == null) {
+            throw new IllegalStateException("Sport and League must be set.");
+        }
+    }
+
+    public void advanceWeek() {
+        league.playNextWeek();
+    }
+
+    public Sport getSport() {
+        return sport;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }
+
+    public League getLeague() {
+        return league;
+    }
+
+    public void setLeague(League league) {
+        this.league = league;
+    }
+
+    public Team getManagedTeam() {
+        return managedTeam;
+    }
+
+    public void setManagedTeam(Team managedTeam) {
+        this.managedTeam = managedTeam;
+    }
+
+
+}
